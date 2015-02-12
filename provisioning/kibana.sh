dbd9df8fd388095c36f8fb1ab71e04656fed298d
Tim Roes <mail@timroes.de>
2015-02-12 22:00:57 +0100

    Upgrade to Kibana 4.0.0-RC1

--- provisioning/kibana.sh
+++ provisioning/kibana.sh
@@ -6,7 +6,7 @@ FILES=/vagrant/provisioning/files
 if [ ! -f /srv/kibana/bin/kibana ]; then
 	# Install kibana
 	mkdir -p /srv/kibana
-	tar -C /srv/kibana -zxf ${FILES}/kibana-4.0.0-beta3.tar.gz --strip 1
+	tar -C /srv/kibana -zxf ${FILES}/kibana-4.0.0-rc1-linux-x64.tar.gz --strip 1
 fi
 
 if [ ! -f /etc/init.d/kibana ]; then
