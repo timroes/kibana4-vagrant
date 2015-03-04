20a8f57c01eba00b57cbc480b3adf6b25e1820bf
Tim Roes <mail@timroes.de>
2015-03-04 19:17:56 +0100

    Provision: update to Kibana 4.0.1

--- provisioning/kibana.sh
+++ provisioning/kibana.sh
@@ -3,9 +3,9 @@
 # Set variables
 FILES=/vagrant/provisioning/files
 if [ "$1" == "64" ]; then
-	KIBANA="kibana-4.0.0-linux-x64.tar.gz"
+	KIBANA="kibana-4.0.1-linux-x64.tar.gz"
 else
-	KIBANA="kibana-4.0.0-linux-x86.tar.gz"
+	KIBANA="kibana-4.0.1-linux-x86.tar.gz"
 fi
 
 if [ ! -f /srv/kibana/bin/kibana ]; then
